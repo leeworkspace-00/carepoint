3f391baf46ea87798d9f98f0122481e86a0abf2b
leeworkspace-00 <leeworkspace-00@gmail.com>
2024-12-26 16:25:29 +0900

    main push test_이경현

--- src/main/java/com/aws/team/controller/LIAController.java
+++ src/main/java/com/aws/team/controller/LIAController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping(value="/lia/")
+@RequestMapping(value="/LIA/")
 public class LIAController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(LIAController.class);
@@ -15,9 +15,9 @@ public class LIAController {
 	@RequestMapping(value="freeBoardList.aws", method=RequestMethod.GET)
 	public String freeBoardList() {
 		
-		System.out.println("freeBoardList.aws µé¾î¿È");
+		System.out.println("freeBoardList.aws ï¿½ï¿½ï¿½ï¿½");
 		
-		return "WEB-INF/lia/freeBoardList"; 
+		return "WEB-INF/LIA/freeBoardList"; 
 	}
 
 }
